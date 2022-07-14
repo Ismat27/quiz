701c3967d54b1f1041057157c28e1aa80972c205
unknown <adegbiteismail27@gmail.com>
2022-07-13 22:20:06 -0700

    set up dynamic styles for previous and next question buttons

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -9,7 +9,17 @@ const Quiz = (props) => {
             <h1>Loading...</h1>
            )
     }
-   
+    
+    const btnStyles = {
+        backgroundColor: index > 0? '#4D5B9E': 'grey',
+        cursor: index > 0? 'pointer' : 'auto'
+    }
+
+    const nextBtnStyle = {
+        backgroundColor: index < props.questions.length - 1? '#4D5B9E': 'grey',
+        cursor: index < props.questions.length - 1 ? 'pointer': 'auto'
+    }
+
     function nextQuestion(currentIndex, totalQuestions) {
         const lastIndex = totalQuestions - 1
         if (currentIndex < lastIndex) {
@@ -37,8 +47,8 @@ const Quiz = (props) => {
                 {props.questionsData[index]}
             </div>
             <div className="nav-btns">
-                <button onClick={() => prevQuestion(index, props.questions.length)}>Previous</button>
-                <button onClick={() => nextQuestion(index, props.questions.length)}>Next</button>
+                <button style={btnStyles} onClick={() => prevQuestion(index, props.questions.length)}>Previous</button>
+                <button style={nextBtnStyle} onClick={() => nextQuestion(index, props.questions.length)}>Next</button>
             </div>
             {props.isAnswering?
                 <button onClick={props.mark}>Submit</button>:
